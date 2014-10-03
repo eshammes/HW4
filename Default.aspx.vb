5af5c9e4fdec466edae207e4eadb52628d32cfec
Evan <evan-hammes@uiowa.edu>
2014-10-03 03:18:08 -0600

    Added clear function to calculator and formatted currency output

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -4,19 +4,31 @@ Partial Class _Default
 
 
     Protected Sub btn_calculate_Click(sender As Object, e As EventArgs) Handles btn_calculate.Click
-        Dim HourlyWage As Decimal = tb_hw.Text
-        Dim NumberHours As Decimal = tb_nh.Text
-        Dim PreTax As Decimal = tb_ptd.Text
-        Dim AfterTax As Decimal = tb_atd.Text
+        Dim HourlyWage As Decimal = Val(tb_hw.Text)
+        Dim NumberHours As Decimal = Val(tb_nh.Text)
+        Dim PreTax As Decimal = Val(tb_ptd.Text)
+        Dim AfterTax As Decimal = Val(tb_atd.Text)
         Dim TaxDeduction As Decimal
+        Dim NetPay As Decimal
         If ((HourlyWage * NumberHours) - PreTax - AfterTax) < 500 Then
             tb_rate.Text = "18%"
             TaxDeduction = (((HourlyWage * NumberHours) - PreTax) * 0.18)
-            tb_np.Text = ((((HourlyWage * NumberHours) - PreTax) - TaxDeduction) - AfterTax)
+            NetPay = ((((HourlyWage * NumberHours) - PreTax) - TaxDeduction) - AfterTax)
+            tb_np.Text = NetPay.ToString("C2")
         ElseIf ((HourlyWage * NumberHours) - PreTax - AfterTax) >= 500 Then
             tb_rate.Text = "22%"
             TaxDeduction = (((HourlyWage * NumberHours) - PreTax) * 0.22)
-            tb_np.Text = ((((HourlyWage * NumberHours) - PreTax) - TaxDeduction) - AfterTax)
+            NetPay = ((((HourlyWage * NumberHours) - PreTax) - TaxDeduction) - AfterTax)
+            tb_np.Text = NetPay.ToString("C2")
         End If
+    End Sub
+
+    Protected Sub btn_clear_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
+        tb_atd.Text = ""
+        tb_hw.Text = ""
+        tb_nh.Text = ""
+        tb_np.Text = ""
+        tb_ptd.Text = ""
+        tb_rate.Text = ""
     End Sub
 End Class
